ddf3b9a9ce13e3c2867fc05652b202ecbfd1e724
chrisortsen <christiano.correa98@gmail.com>
2021-05-03 05:04:37 -0300

    ativ_fix_swing_gui_ChristianoCorrea

---
+++ ativ_fix_swing_gui_ChristianoCorrea/src/ativ_fix_swing_gui_ChristianoCorrea/Principal.java
@@ -0,0 +1,13 @@
+package ativ_fix_swing_gui_ChristianoCorrea;
+
+public class Principal {
+
+	public static void main(String[] args) {
+		
+        new ProgramaCalculadora();
+		
+
+	}
+
+}
+
---
+++ ativ_fix_swing_gui_ChristianoCorrea/src/ativ_fix_swing_gui_ChristianoCorrea/Teste01.java
@@ -0,0 +1,43 @@
+package ativ_fix_swing_gui_ChristianoCorrea;
+
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+
+public class Teste01 {
+
+	private JFrame frame;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					Teste01 window = new Teste01();
+					window.frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the application.
+	 */
+	public Teste01() {
+		initialize();
+	}
+
+	/**
+	 * Initialize the contents of the frame.
+	 */
+	private void initialize() {
+		frame = new JFrame();
+		frame.setBounds(100, 100, 450, 300);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+	}
+
+}
